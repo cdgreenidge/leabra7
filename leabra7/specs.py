21bc99716f910d814e7a1c3294d22eddc7d1e4e5
Daniel Greenidge <git@danielgreenidge.com>
2017-10-31 11:56:03 -0400

    Test for nonexistent attributes in spec constructors.

--- leabra7/specs.py
+++ leabra7/specs.py
@@ -6,4 +6,5 @@ from typing import Any
 class Spec:
     def __init__(self, **kwargs: Dict[str, Any]) -> None:
         for name, value in kwargs.items():
+            getattr(self, name)  # Throw exception if it doesn't exist
             setattr(self, name, value)
--- tests/test_spec.py
+++ tests/test_spec.py
@@ -1,7 +1,18 @@
 """Test specs.py"""
+import pytest
+
 from leabra7 import specs as sp
 
 
+class Foo(sp.Spec):
+    a = 3
+
+
 def test_spec_sets_attributes_from_the_constructor():
-    spec = sp.Spec(a=3)
-    assert spec.a == 3
+    spec = Foo(a=4)
+    assert spec.a == 4
+
+
+def test_spec_throws_an_error_if_you_set_a_nonexistent_attribute():
+    with pytest.raises(AttributeError):
+        Foo(b=4)
