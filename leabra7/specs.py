e57bb36b5301e387acb4722e1cbb3041e3ae6f47
Daniel Greenidge <git@danielgreenidge.com>
2017-10-31 12:51:29 -0400

    Add input and inhibition parameters to specs.

--- leabra7/specs.py
+++ leabra7/specs.py
@@ -4,6 +4,9 @@ from typing import Any
 
 
 class Spec:
+    # Global integration time constant
+    integ = 1.0
+
     def __init__(self, **kwargs: Dict[str, Any]) -> None:
         for name, value in kwargs.items():
             getattr(self, name)  # Throw exception if it doesn't exist
@@ -13,5 +16,19 @@ class Spec:
         return self.__dict__ == other.__dict__
 
 
+class LayerSpec(Spec):
+    # Feedforward inhibition multiplier
+    ff = 1.0
+    # Feedforward inhibition offset
+    ff0 = 0.1
+    # Feedback inhibition multiplier
+    fb = 1.0
+    # Feedback inhibition integration time constant
+    fb_dt = 1 / 1.4
+    # Global (feedforward + feedback) inhibition multiplier
+    gi = 1.8
+
+
 class UnitSpec(Spec):
-    pass
+    # Net input integration time constant
+    net_dt = 1 / 1.4
