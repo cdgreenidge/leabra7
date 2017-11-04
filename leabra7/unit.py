7255eb9c26d48fbcec39dfb2486329e2258d9aaf
Daniel Greenidge <git@danielgreenidge.com>
2017-11-04 11:18:39 -0400

    Add noisy x / (x + 1) function.

--- leabra7/unit.py
+++ leabra7/unit.py
@@ -1,8 +1,86 @@
 """A single computational unit, similar to a neuron."""
+from typing import Any
+from typing import Callable
+
+import numpy as np  # type: ignore
+import scipy.interpolate  # type: ignore
+
 from leabra7 import specs
+
+# The next few functions deal with the noisy x/(x + 1) activation
+# function. The actual unit class is farther down
+
+
+def gaussian(res: float, std: float) -> Any:
+    """Returns a Gaussian PDF with mean 0 and variance std^2, in an array of
+    resolution res.
+
+    """
+    if std < 1e-3:
+        raise ValueError("Gaussian std cannot be less than 1e-3.")
+    if res > 3.0 * std:
+        raise ValueError("Gaussian res cannot be greater than 3.0 * std.")
+    xlim = 3.0 * std
+    xs = np.arange(-xlim, xlim + res, res)
+    # Not sure why variance is half the normal size
+    gauss = np.exp(-(xs * xs) / (std * std))
+    # We need to normalize with a sum because our tails do not go to infinity
+    return gauss / sum(gauss)
+
+
+def xx1(res: float, xmin: float, xmax: float) -> Any:
+    """Returns the xx1 function from xmin to xmax in an array with resolution
+    res.
+
+    """
+    xs = np.arange(xmin, xmax, res)
+    gain = 40
+    u = gain * np.maximum(xs, 0.0)
+    return u / (u + 1)
+
+
+def nxx1_table() -> Any:
+    """Returns an (x, y) tuple where x and y are arrays holding the x and f(x)
+    values of the xx1 function convolved with Gaussian noise.
+
+    """
+    res = 0.001  # Resolution of x-axis
+    std = 0.01  # Standard deviation of the Gaussian noise
+
+    rng = 3 * std
+    xmin = -2 * rng
+    xmax = rng + 1.0 + res
+    xs = np.arange(xmin, xmax, res)
+    ys = xx1(res, xmin, xmax)
+    conv = np.convolve(ys, gaussian(res, std), mode="same")
+
+    xs_valid = np.arange(-rng, 1.0 + res, res)
+    conv = conv[np.searchsorted(xs, xs_valid[0], side="left"):
+                np.searchsorted(xs, xs_valid[-1], side="left") + 1]
+    return (xs_valid, conv)
+
+
+def nxx1_interpolator() -> Callable[[float], float]:
+    """Returns a function that caches and interpolates the noisy xx1 lookup
+    table.
+
+    """
+    xs, conv = nxx1_table()
+    f = scipy.interpolate.interp1d(xs, conv, copy=False)
+
+    def nxx1(x: float) -> float:
+        if x < xs[0]:
+            return conv[0]
+        elif x > xs[-1]:
+            return conv[-1]
+        return f(x)
+
+    return nxx1
 
 
 class Unit:
+    nxx1 = staticmethod(nxx1_interpolator())
+
     def __init__(self, spec: specs.UnitSpec = None) -> None:
         if spec is None:
             self.spec = specs.UnitSpec()
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,8 +1,73 @@
 """Test unit.py"""
+import math
+
+import numpy as np
+import pytest
+
 from leabra7 import specs as sp
 from leabra7 import unit as un
 
 
+# Test un.gaussian(res, std)
+def test_gaussian_returns_a_probability_density():
+    assert math.isclose(sum(un.gaussian(res=0.001, std=0.01)), 1)
+
+
+def test_gaussian_raises_an_error_if_std_is_too_small():
+    with pytest.raises(ValueError):
+        un.gaussian(res=0.001, std=0.9e-3)
+
+
+def test_gaussian_raises_an_error_if_res_is_too_big():
+    with pytest.raises(ValueError):
+        un.gaussian(res=3, std=0.9)
+
+
+# Test un.xx1(res, xmin, xmax)
+def test_xx1_returns_the_correct_array():
+    # This is a regression test on Fabien Benreau's prototype
+    reference = np.load("tests/xx1.npy")
+    assert np.allclose(un.xx1(0.001, -3, 3), reference)
+
+
+@pytest.fixture(scope="module")
+def nxx1_table():
+    return un.nxx1_table()
+
+
+# Test un.nxx1_table()
+def test_nxx1_table_returns_the_correct_arrays(nxx1_table):
+    # This is a regression test on Fabien Benreau's prototype
+    file = np.load("tests/nxx1.npz")
+    reference_xs = file["xs"]
+    reference_conv = file["conv"]
+    file.close()
+    xs, conv = nxx1_table
+    assert np.allclose(reference_xs, xs)
+    assert np.allclose(reference_conv, conv)
+
+
+# Test nxx1_interpolator() and nxx1()
+def test_nxx1_equals_the_lookup_table(nxx1_table):
+    unit = un.Unit()
+    xs, conv = nxx1_table
+    for i in range(0, xs.size, 50):
+        assert math.isclose(unit.nxx1(xs[i]), conv[i])
+
+
+def test_nxx1_equals_the_min_table_value_outside_the_min_boundary(nxx1_table):
+    unit = un.Unit()
+    xs, conv = nxx1_table
+    assert unit.nxx1(xs[0] - 1) == conv[0]
+
+
+def test_nxx1_equals_the_max_table_value_outside_the_max_boundary(nxx1_table):
+    unit = un.Unit()
+    xs, conv = nxx1_table
+    assert unit.nxx1(xs[-1] + 1) == conv[-1]
+
+
+# Test Unit class
 def test_unit_init_uses_the_spec_you_pass_it():
     unit = un.Unit(spec=3)
     assert unit.spec == 3
@@ -13,12 +78,12 @@ def test_unit_init_can_make_a_defaut_spec_for_you():
     assert unit.spec == sp.UnitSpec()
 
 
-def test_unit_has_0_raw_net_input_at_first():
+def test_unit_has_0_raw_un_input_at_first():
     unit = un.Unit()
     assert unit.net_raw == 0
 
 
-def test_unit_can_add_inputs_to_the_raw_net_input():
+def test_unit_can_add_inputs_to_the_raw_un_input():
     unit = un.Unit()
     unit.add_input(3)
     assert unit.net_raw == 3
