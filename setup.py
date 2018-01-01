d18a707d31710ceae181f73b92a94564989139f5
Daniel Greenidge <git@danielgreenidge.com>
2018-01-01 10:39:24 -0700

    Add Pandas as a dependency.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="Leabra7",
     version="0.1.dev1",
     packages=find_packages(exclude=["docs", "tests"]),
-    install_requires=["numpy", "scipy"],
+    install_requires=["numpy", "pandas", "scipy"],
     # We put the test requirements in the extra requirements also to access
     # them from outside tools, e.g. pip3 install .[dev]
     extras_require={
