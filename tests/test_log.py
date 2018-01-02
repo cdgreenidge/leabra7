e17670f13cb6e64199aa6c41b8f30faf0ad70bbe
Daniel Greenidge <git@danielgreenidge.com>
2018-01-01 19:15:14 -0700

    Reword log test names.

--- tests/test_log.py
+++ tests/test_log.py
@@ -5,21 +5,21 @@ from leabra7 import log
 
 
 # Test log.DataFrameBuffer
-def test_if_there_is_a_new_attr_it_uses_nones_to_fill_in_missing_values():
+def test_dataframebuffer_can_record_new_attributes():
     dfb = log.DataFrameBuffer()
     dfb.append([("a", 1)])
     dfb.append([("a", 2), ("b", 2)])
     assert dfb.buf == {"a": [1, 2], "b": [None, 2]}
 
 
-def test_if_the_attr_list_is_incomplete_it_pads_the_missing_attrs_with_nones():
+def test_dataframebuffer_can_record_partial_rows():
     dfb = log.DataFrameBuffer()
     dfb.append([("a", 1), ("b", 2)])
     dfb.append([("c", 3)])
     assert dfb.buf == {"a": [1, None], "b": [2, None], "c": [None, 3]}
 
 
-def test_it_converts_the_buffer_to_a_dataframe_on_flush():
+def test_dataframebuffer_can_serialize_itself_to_a_dataframe():
     dfb = log.DataFrameBuffer()
     dfb.append([("a", 1), ("b", 3)])
     dfb.append([("a", 2), ("b", 4)])
@@ -35,7 +35,7 @@ class ObjToLog:
         return getattr(self, attr)
 
 
-def test_it_can_record_attributes_from_an_object():
+def test_logger_can_record_attributes_from_an_object():
     obj = ObjToLog()
     logger = log.Logger(obj, ["a"])
     obj.a = 3
