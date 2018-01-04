a7388a5169f96d29725eea5c58b831da2f6ddc57
Daniel Greenidge <git@danielgreenidge.com>
2018-01-04 15:41:42 -0700

    Add scaffolding for spec validation.

--- leabra7/specs.py
+++ leabra7/specs.py
@@ -1,23 +1,85 @@
 """Classes that bundle simulation parameters."""
+import abc
+import math
 from typing import Any
 from typing import Dict
 from typing import Tuple  # noqa pylint: disable=W0611
 
 
-class Spec:
+class ValidationError(Exception):
+    """Raised when an object fails validation checks."""
+    pass
+
+
+class Spec(metaclass=abc.ABCMeta):
+    """Specs are record classes that hold parameter values.
+
+    Every parameter in a spec has a default value, but you can override these
+    default values using constructor keword arguments. For example,
+    `UnitSpec()` will create a unit spec with all default values, but
+    `UnitSpec(spk_thr=0.3)` will create a unit spec with all default values
+    except for `spk_thr`, which is set to `0.3`.
+
+    Specs can be modified at runtime, so before using a spec given
+    to you by a user, always call the `validate()` method.
+
+    Raises:
+        ValueError: if you attempt to override a property that does not exist
+            in this spec.
+
+    """
     # Global integration time constant
     integ = 1.0
 
     def __init__(self, **kwargs: Dict[str, Any]) -> None:
         for name, value in kwargs.items():
-            getattr(self, name)  # Throw exception if it doesn't exist
+            if not hasattr(self, name):
+                raise ValueError("{0} is not a valid parameter name for this "
+                                 "spec.".format(name))
             setattr(self, name, value)
 
     def __eq__(self, other: object) -> bool:
         return self.__dict__ == other.__dict__
 
+    def assert_in_range(self, attr: str, low: float, high: float) -> None:
+        """Asserts that an attribute is in a closed interval.
+
+        Args:
+            attr: The attribute to check.
+            low: The lower bound of the interval.
+            high: The upper bound of the interval.
+
+        Raises:
+            ValidationError: If the attribute lies outside of [low, high].
+
+        """
+        if low > high:
+            raise ValueError("low must be less than or equal to high.")
+        if not low <= getattr(self, attr) <= high:
+            msg = "{0} must be in the interval [{1}, {2}].".format(
+                attr, low, high)
+            raise ValidationError(msg)
+
+    def assert_not_nan(self, attr: str) -> None:
+        """Asserts that an attribute is not NaN.
+
+        Args:
+            attr: The attribute to check.
+
+        Raises:
+            ValidationError: If the attribute is Nan.
+
+        """
+        if math.isnan(getattr(self, attr)):
+            raise ValidationError("Attribute {0} is NaN.".format(attr))
+
+    @abc.abstractmethod
+    def validate(self) -> None:
+        """Raises ValidationError if any parameter is out of range."""
+
 
 class UnitSpec(Spec):
+    """Spec for unit objects."""
     # Excitation (net input) reversal potential
     e_rev_e = 1
     # Inhibitory reversal potential
@@ -41,6 +103,10 @@ class UnitSpec(Spec):
     # Adaption current integration time constant
     adapt_dt = 1 / 144
 
+    def validate(self) -> None:
+        """Overrides `Spec.validate`."""
+        raise NotImplementedError
+
 
 class LayerSpec(Spec):
     # Feedforward inhibition multiplier
@@ -60,10 +126,18 @@ class LayerSpec(Spec):
     # Layers need to know how to construct their units
     unit_spec = UnitSpec()
 
+    def validate(self) -> None:
+        """Overrides `Spec.validate`."""
+        raise NotImplementedError
+
 
 class ConnSpec(Spec):
-    pass
+    def validate(self) -> None:
+        """Overrides `Spec.validate`."""
+        raise NotImplementedError
 
 
 class ProjnSpec(Spec):
-    pass
+    def validate(self) -> None:
+        """Overrides `Spec.validate`."""
+        raise NotImplementedError
--- tests/test_spec.py
+++ tests/test_spec.py
@@ -7,6 +7,9 @@ from leabra7 import specs as sp
 class Foo(sp.Spec):
     a = 3
 
+    def validate(self) -> None:
+        pass
+
 
 def test_spec_sets_attributes_from_the_constructor():
     spec = Foo(a=4)
@@ -14,7 +17,7 @@ def test_spec_sets_attributes_from_the_constructor():
 
 
 def test_spec_throws_an_error_if_you_set_a_nonexistent_attribute():
-    with pytest.raises(AttributeError):
+    with pytest.raises(ValueError):
         Foo(b=4)
 
 
@@ -24,3 +27,24 @@ def test_spec_supports_equality_comparison():
     assert a == b
     a.a = 4
     assert a != b
+
+
+def test_spec_can_check_if_an_attribute_is_in_range():
+    a = Foo()
+    a.assert_in_range("a", low=0, high=4)
+    with pytest.raises(sp.ValidationError):
+        a.assert_in_range("a", low=0, high=1)
+
+
+def test_spec_assert_in_range_checks_the_bounds_make_sense():
+    a = Foo()
+    with pytest.raises(ValueError):
+        a.assert_in_range("a", low=3, high=2)
+
+
+def test_spec_can_check_if_an_attribute_is_nan():
+    a = Foo()
+    a.assert_not_nan("a")
+    a.a = float("nan")
+    with pytest.raises(sp.ValidationError):
+        a.assert_not_nan("a")
