90463bc077fbbe78396ceb22f388228bb83c28c6
Daniel Greenidge <git@danielgreenidge.com>
2018-01-10 18:05:56 -0500

    Productionize net.py.

--- leabra7/net.py
+++ leabra7/net.py
@@ -12,6 +12,8 @@ from leabra7 import specs
 
 
 class Net:
+    """A leabra7 network. This is the main class."""
+
     def __init__(self) -> None:
         self.objs = {}  # type: Dict[str, Any]
         self.layers = []  # type: List[layer.Layer]
@@ -21,6 +23,20 @@ class Net:
 
     def new_layer(self, name: str, size: int,
                   spec: specs.LayerSpec = None) -> None:
+        """Adds a new layer to the network.
+
+        Args:
+            name: The name of the layer.
+            size: How many units the layer should have.
+            spec: The layer specification.
+
+        Raises:
+            spec.ValidationError: If the spec contains an invalid parameter
+                value.
+
+        """
+        if spec is not None:
+            spec.validate()
         lr = layer.Layer(name, size, spec)
         self.layers.append(lr)
         self.objs[lr.name] = lr
@@ -33,13 +49,40 @@ class Net:
                   pre: str,
                   post: str,
                   spec: specs.ProjnSpec = None) -> None:
-        pre_lr = self.objs[pre]
-        post_lr = self.objs[post]
+        """Adds a new projection to the network.
+
+        Args:
+            name: The name of the projection.
+            pre: The name of the sending layer.
+            post: The name of the receiving layer.
+            spec: The projection specification.
+
+        Raises:
+            ValueError: If `pre` or `post` do not match any existing layer
+                name.
+            spec.ValidationError: If the spec contains an invalid parameter
+                value.
+
+        """
+        if spec is not None:
+            spec.validate()
+
+        try:
+            pre_lr = self.objs[pre]
+        except KeyError:
+            raise ValueError("No layer found with name {0}.".format(pre))
+
+        try:
+            post_lr = self.objs[post]
+        except KeyError:
+            raise ValueError("No layer found with name {0}.".format(post))
+
         pr = projn.Projn(name, pre_lr, post_lr, spec)
         self.projns.append(pr)
         self.objs[pr.name] = pr
 
     def cycle(self) -> None:
+        """Cycles the network."""
         for lg in self.cycle_loggers:
             lg.record()
 
@@ -50,6 +93,18 @@ class Net:
             pr.flush()
 
     def logs(self, freq: str, name: str) -> None:
+        """Retrieves logs for an object in the network.
+
+        Args:
+            freq: The frequency at which the desired logs were recorded. One
+                of `["cycle"]`.
+            name: The name of the object for which the logs were recorded.
+
+        Raises:
+            ValueError: If the frequency name is invalid, or if no logs were
+                recorded for the desired object.
+
+        """
         freq_names = {"cycle": self.cycle_loggers}
         try:
             freq_loggers = freq_names[freq]
--- tests/test_net.py
+++ tests/test_net.py
@@ -1,1 +1,69 @@
 """Test net.py"""
+import pytest
+
+from leabra7 import net
+from leabra7 import specs
+
+
+def test_a_new_layer_validates_its_spec():
+    n = net.Net()
+    with pytest.raises(specs.ValidationError):
+        n.new_layer("layer1", 3, spec=specs.LayerSpec(integ=-1))
+
+
+def test_you_can_create_a_layer_with_a_default_spec():
+    n = net.Net()
+    n.new_layer("layer1", 3)
+
+
+def test_a_new_projn_validates_its_spec():
+    n = net.Net()
+    n.new_layer("layer1", 3)
+    n.new_layer("layer2", 3)
+    with pytest.raises(specs.ValidationError):
+        n.new_projn(
+            "projn1", "layer1", "layer2", spec=specs.ProjnSpec(integ=-1))
+
+
+def test_you_can_create_a_projn_with_a_default_spec():
+    n = net.Net()
+    n.new_layer("layer1", 3)
+    n.new_layer("layer2", 3)
+    n.new_projn("projn1", "layer1", "layer2")
+
+
+def test_projn_checks_if_the_sending_layer_name_is_valid():
+    n = net.Net()
+    n.new_layer("layer2", 3)
+    with pytest.raises(ValueError):
+        n.new_projn("projn1", "layer1", "layer2")
+
+
+def test_projn_checks_if_the_receiving_layer_name_is_valid():
+    n = net.Net()
+    n.new_layer("layer1", 3)
+    with pytest.raises(ValueError):
+        n.new_projn("projn1", "layer1", "layer2")
+
+
+# Right now, it's difficult to test net.cycle(), because it's the core of the
+# stateful updates. Eventually, we'll add some regression tests for it.
+
+
+def test_net_logs_checks_whether_the_frequency_name_is_valid():
+    n = net.Net()
+    with pytest.raises(ValueError):
+        n.logs(freq="guitar", name="layer1")
+
+
+def test_net_logs_checks_whether_the_object_name_is_valid():
+    n = net.Net()
+    with pytest.raises(ValueError):
+        n.logs(freq="cycle", name="layer1")
+
+
+def test_you_can_retrieve_the_logs_for_a_layer():
+    n = net.Net()
+    n.new_layer("layer1", 3, spec=specs.LayerSpec(log_on_cycle=("avg_act", )))
+    n.cycle()
+    assert "avg_act" in n.logs("cycle", "layer1").columns
