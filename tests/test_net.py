b4555e7401c12ac5f0b28a647d8c671975a2896d
Daniel Greenidge <git@danielgreenidge.com>
2018-02-07 11:21:58 -0500

    Fix bug in network layer name validation.
    It was possible to give the name of an object that existed in the
    network, but that was not a layer, and have it pass validation. Now we
    check both that the object exists, and that it is a layer.

--- tests/test_net.py
+++ tests/test_net.py
@@ -8,8 +8,21 @@ from leabra7 import specs
 def test_the_network_can_check_if_an_object_exists_within_it():
     n = net.Net()
     n.new_layer("layer1", 3)
-    assert n._assert_obj_exits("layer1")
-    with pytest.raises(AssertionError)
+    n._validate_obj_name("layer1")
+    with pytest.raises(ValueError):
+        n._validate_obj_name("whales")
+
+
+def test_the_network_can_check_if_an_object_is_a_layer():
+    n = net.Net()
+    n.new_layer("layer1", 3)
+    n.new_layer("layer2", 3)
+    n.new_projn("proj1", "layer1", "layer2")
+    with pytest.raises(ValueError):
+        n._validate_layer_name("whales")
+    with pytest.raises(ValueError):
+        n._validate_layer_name("proj1")
+
 
 def test_a_new_layer_validates_its_spec():
     n = net.Net()
