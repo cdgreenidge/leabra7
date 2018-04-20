fb95bd73113b14aed7bfbe5767af4a5db8366370
Daniel Greenidge <git@danielgreenidge.com>
2018-04-20 12:09:14 -0400

    Add torch to package requirements.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name="Leabra7",
     version="0.1.dev1",
     packages=find_packages(exclude=["docs", "tests"]),
-    install_requires=["numpy", "pandas", "scipy"],
+    install_requires=["numpy", "pandas", "scipy", "torch"],
     tests_require=tests_require,
     # We put the test requirements in the extra requirements also to access
     # them from outside tools, e.g. pip3 install .[dev]
