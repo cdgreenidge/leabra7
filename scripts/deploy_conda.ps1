d8abb7e59709902a5d477097cc46924f4b812970
Daniel Greenidge <daniel@danielgreenidge.com>
2018-07-11 11:28:24 -0600

    Check for pull requests in Appveyor deployment script

--- scripts/deploy_conda.ps1
+++ scripts/deploy_conda.ps1
@@ -1,5 +1,10 @@
+if (Test-Path env:APPVEYOR_PULL_REQUEST_NUMBER) {
+   echo "Pull request detected. Aborting deployment..."
+   exit 0
+}
+
 echo "Finding package path..."
-$packagePath = (conda build .\scripts\meta.yaml --output)
+$packagePath = (conda build scripts/meta.yaml --output)
 
 echo "Uploading package..."
 anaconda --token $env:CONDA_TOKEN upload $packagePath
