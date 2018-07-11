5d4da142fcb81c7e2b5a3210fbd6f90baa5dea2c
Daniel Greenidge <daniel@danielgreenidge.com>
2018-07-11 13:08:40 -0600

    Disable AppVeyor on non-master branches (v2.0)

--- scripts/deploy_conda.ps1
+++ scripts/deploy_conda.ps1
@@ -1,6 +1,9 @@
-if (Test-Path env:APPVEYOR_PULL_REQUEST_NUMBER) {
-   echo "Pull request detected. Aborting deployment..."
-   exit 0
+If (Test-Path env:APPVEYOR_PULL_REQUEST_NUMBER) {
+    echo "Pull request detected. Aborting deployment..."
+    exit 0
+} ElseIf ($env:APPVEYOR_REPO_BRANCH -ne "master") {
+    echo "Not on master. Aborting deployment..."
+    exit 0
 }
 
 echo "Finding package path..."
