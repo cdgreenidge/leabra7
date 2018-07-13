5327b714be69144d496272ea90750f371d1371af
Daniel Greenidge <daniel@danielgreenidge.com>
2018-07-12 20:53:03 -0600

    Add basic program tree.

--- leabra7/program.py
+++ leabra7/program.py
@@ -1,48 +1,107 @@
 """Components for the AST that a leabra7 network can run."""
 import abc
+from typing import Any
+from typing import Iterable
+from typing import Sequence
+import uuid
 
 
 class Node:
+    """A node in a rooted, directed tree.
+
+    Args:
+      children: An iterable of the children of the node.
+      name: The name of the node. Defaults to a unique auto-generated string.
+
+    """
+
+    def __init__(self, children: Sequence["Node"] = (),
+                 name: str = None) -> None:
+        try:
+            iter(children)
+        except TypeError:
+            raise ValueError("children argument must be iterable.")
+
+        if name is None:
+            self.name = uuid.uuid4().hex
+        else:
+            self.name = name
+
+        self.children = children
+
+    def preorder(self) -> Iterable["Node"]:
+        """Returns a DFS preordering of the node's children.
+
+        Children are ordered left-to-right. We also assume that the graph
+        is a tree (so cycles will cause an infinite loop).
+
+        """
+        stack = [self]
+        while stack:
+            n = stack.pop()
+            stack.extend(reversed(n.children))
+            yield n
+
+    def atomic_stream(self) -> Iterable["Node"]:
+        """Returns a stream of the node's children that are atomic events.
+
+        The atomic events are ordered using a L-R DFS preodering.
+
+        """
+        return (i for i in self.preorder() if isinstance(i, AtomicEvent))
+
+
+class Program(Node):
+    """A program is a tree of events that can be executed by the network."""
     pass
 
 
-class AtomicEvent():
+class AtomicEvent(Node):
     """An atomic event is an event that the network can execute directly.
+
+    Because the network can execute it directly, it cannot have child nodes.
 
     It's ok to use isinstance() to check if what kind of AtomicEvent you get,
     because we're basically mimicing an algebraic datatype.
 
+    Raises:
+      ValueError: if children is not an empty sequence.
+
     """
-    pass
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        if self.children:
+            raise ValueError("Atomic events cannot have children.")
 
 
-class Cycle(Node, AtomicEvent):
+class Cycle(AtomicEvent):
     """The event that cycles the network."""
     pass
 
 
-class BeginPlusPhase(Node, AtomicEvent):
+class BeginPlusPhase(AtomicEvent):
     """The event that begins the plus phase in a trial."""
     pass
 
 
-class EndPlusPhase(Node, AtomicEvent):
+class EndPlusPhase(AtomicEvent):
     """The event that ends the plus phase in a trial."""
     pass
 
 
-class BeginMinusPhase(Node, AtomicEvent):
+class BeginMinusPhase(AtomicEvent):
     """The event that begins the minus phase in a trial."""
     pass
 
 
-class EndMinusPhase(Node, AtomicEvent):
+class EndMinusPhase(AtomicEvent):
     """The event that ends the minus phase in a trial."""
     pass
 
 
-class ForceLayer(Node, AtomicEvent):
-    """The event that forces a layer."""
+class HardClamp(AtomicEvent):
+    """The event that hard clamps a layer."""
     pass
 
 
---
+++ tests/test_program.py
@@ -0,0 +1,53 @@
+"""Tests program.py"""
+import pytest
+
+from leabra7 import program as pr
+
+
+def test_node_checks_that_children_is_an_iterable() -> None:
+    with pytest.raises(ValueError):
+        pr.Node(children=(pr.Node()))
+
+
+def test_nodes_can_be_names() -> None:
+    a = pr.Node(name="bob")
+    assert a.name == "bob"
+
+
+def test_nodes_provide_default_unique_names() -> None:
+    n = 25
+    assert len(set(pr.Node().name for i in range(n))) == n
+
+
+def test_nodes_can_provide_a_dfs_preordering_of_their_children() -> None:
+    # yapf: disable
+    root = pr.Node(name="A", children=(
+        pr.Node(name="B", children=(
+            pr.Node(name="D"),
+        )),
+        pr.Node(name="C", children=(
+            pr.Node(name="E"),
+        ))
+    ))
+    # yapf: enable
+    order = root.preorder()
+    names = list(u.name for u in order)
+    assert names == ["A", "B", "D", "C", "E"]
+
+
+def test_nodes_can_stream_their_atomic_event_children() -> None:
+    root = pr.Node(
+        name="A",
+        children=(pr.Node(
+            name="B",
+            children=(pr.BeginPlusPhase(name="D"), pr.Cycle(name="E"))),
+                  pr.Node(name="C", children=(pr.EndPlusPhase(name="F"), ))))
+    assert list(u.name for u in root.atomic_stream()) == ["D", "E", "F"]
+
+
+def test_atomic_events_raise_valueerror_if_they_are_assigned_children(
+) -> None:
+    with pytest.raises(ValueError):
+        pr.Cycle(children=(pr.Node(), ))
+    with pytest.raises(ValueError):
+        pr.BeginPlusPhase(children=(pr.Node(), ))
