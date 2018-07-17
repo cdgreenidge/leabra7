ee8d0fed626f431c925b8b145d9ffab8c76d2227
Noam Miller <noammiller99@gmail.com>
2018-07-17 16:34:08 -0400

    Added streamlined importing of distribution classes

--- leabra7/__init__.py
+++ leabra7/__init__.py
@@ -1,1 +1,7 @@
 """Implements the LEABRA algorithm, v7.0"""
+
+from leabra7.rand import Exponential
+from leabra7.rand import Gaussian
+from leabra7.rand import LogNormal
+from leabra7.rand import Scalar
+from leabra7.rand import Uniform
