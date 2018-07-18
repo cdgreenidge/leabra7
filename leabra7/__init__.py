44100b92f12a615cde43a08109fc55f730698d64
Noam Miller <noammiller99@gmail.com>
2018-07-18 14:59:02 -0400

    Removed event imports

--- leabra7/__init__.py
+++ leabra7/__init__.py
@@ -1,13 +1,5 @@
 """Implements the LEABRA algorithm, v7.0"""
 
-from leabra7.events import Cycle
-from leabra7.events import BeginPlusPhase
-from leabra7.events import EndPlusPhase
-from leabra7.events import BeginMinusPhase
-from leabra7.events import EndMinusPhase
-from leabra7.events import HardClamp
-from leabra7.events import Unclamp
-
 from leabra7.rand import Scalar
 from leabra7.rand import Uniform
 from leabra7.rand import Gaussian
