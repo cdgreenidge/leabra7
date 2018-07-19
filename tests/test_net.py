fe57382e4490cbfbe35b93706f773d99f5e7b4da
Daniel Greenidge <daniel@danielgreenidge.com>
2018-07-19 15:08:29 -0600

    Revert "Moved kWTA tests to the net module"
    This reverts commit 38189155aca80c8e5bb5d79fafc1398dfae267cd.

--- tests/test_net.py
+++ tests/test_net.py
@@ -1,13 +1,11 @@
 """Test net.py"""
 import math
 
-import numpy as np
 import pytest
 
 from leabra7 import events
 from leabra7 import net
 from leabra7 import specs
-from leabra7 import rand
 
 
 def test_the_network_can_check_if_an_object_exists_within_it() -> None:
@@ -54,65 +52,6 @@ def test_a_new_layer_validates_its_spec() -> None:
 def test_you_can_create_a_layer_with_a_default_spec() -> None:
     n = net.Net()
     n.new_layer("layer1", 3)
-
-
-# Test kWTA Inhibition
-def test_kwta_suppresses_all_but_k_units() -> None:
-    for p in np.linspace(0.0, 1.0, 10):
-        n = net.Net()
-        n.new_layer(name="lr1", size=1)
-
-        lr2_spec = specs.LayerSpec(
-            inhibition_type="kwta",
-            kwta_pct=p,
-            log_on_cycle=("unit_act", ),
-            unit_spec=specs.UnitSpec(adapt_dt=0, spike_gain=0))
-        n.new_layer(name="lr2", size=10, spec=lr2_spec)
-
-        pr1_spec = specs.ProjnSpec(dist=rand.Uniform(low=0.25, high=0.75))
-        n.new_projn("proj1", "lr1", "lr2", pr1_spec)
-
-        pr2_spec = specs.ProjnSpec(dist=rand.Uniform(low=0.25, high=0.75))
-        n.new_projn("proj2", "lr1", "lr2", pr2_spec)
-
-        n.clamp_layer("lr1", [1])
-        for i in range(50):
-            n.cycle()
-
-        logs = n.logs("cycle", "lr2").parts
-        acts = logs[logs.time == 49]["act"]
-
-        assert (acts > 0.5).sum() == max(1, int(round(10 * p)))
-
-
-# TODO: precisely define this
-def test_kwta_avg_suppresses_all_but_k_units() -> None:
-    for p in np.linspace(0.4, 1.0, 10):
-        n = net.Net()
-        n.new_layer(name="lr1", size=1)
-
-        lr2_spec = specs.LayerSpec(
-            inhibition_type="kwta_avg",
-            kwta_pct=p,
-            log_on_cycle=("unit_act", ),
-            unit_spec=specs.UnitSpec(adapt_dt=0, spike_gain=0))
-        n.new_layer(name="lr2", size=10, spec=lr2_spec)
-
-        pr1_spec = specs.ProjnSpec(dist=rand.Uniform(low=0.25, high=0.75))
-        n.new_projn("proj1", "lr1", "lr2", pr1_spec)
-
-        pr2_spec = specs.ProjnSpec(dist=rand.Uniform(low=0.25, high=0.75))
-        n.new_projn("proj2", "lr1", "lr2", pr2_spec)
-
-        n.clamp_layer("lr1", [1])
-        for i in range(100):
-            n.cycle()
-
-        logs = n.logs("cycle", "lr2").parts
-        acts = logs[logs.time == 99]["act"]
-
-        # Can't exactly be exact about how many winners selected
-        # assert (acts > 0.5).sum() == max(1, int(round(10 * p)))
 
 
 def test_you_can_hard_clamp_a_layer() -> None:
